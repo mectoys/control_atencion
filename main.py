54212f2654985995ba42760b9d1da70168665837
mectoys <mectoys2013@gmail.com>
2023-06-19 21:26:40 -0500

    Codificación del Modelo Attention_Control.py Agregar

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 
 from flask import Flask
 from config import config
+from scr.models.entities.Attention_Control import Attention_Control
 from scr.models.model_Attention_Control import model_Attention_Control
 app= Flask(__name__)
 
 
+
 if __name__ == '__main__':
-    model_Attention_Control.get_attention_control_list()
+    #model_Attention_Control.get_attention_control_list()
     app.config.from_object(config['development'])
+
     app.run()
+
--- scr/models/model_Attention_Control.py
+++ scr/models/model_Attention_Control.py
@@ -4,6 +4,7 @@ from scr.database.dbconnect import get_connection
 class model_Attention_Control:
 
     @classmethod
+    #Listar
     def get_attention_control_list(cls):
         connection = get_connection()
         with connection.cursor() as cursor:
@@ -12,5 +13,20 @@ class model_Attention_Control:
             cursor.execute(SQL)
             listattention_ctrl = cursor.fetchall()
         connection.close()
-        print(listattention_ctrl)
+        #print(listattention_ctrl)
         return listattention_ctrl
+    #Insertar
+
+    @classmethod
+    def add_attention_control(cls,attention_control):
+        connection =get_connection()
+        with connection.cursor()as cursor:
+            #SQLINJECTION EVITA CON LA CONSULTA PARAMETRIZADA
+            SQLINSERT="INSERT INTO Attention_Control(FECHA, NOMBRES, HORA_INGRESO, HORA_SALIDA, POLO_GIFT, KEYCHAIN_GIFT, CATALOG_BOOK)"\
+                " VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            values=(attention_control.fecha, attention_control.nombres, attention_control.hora_ingreso, attention_control.hora_salida,
+                    attention_control.polo_gift, attention_control.keychain_gift, attention_control.catalog_book)
+            cursor.execute(SQLINSERT,values)
+            affected_rows= cursor.rowcount
+            connection.commit()
+        return affected_rows
