4215de4496b94b71ef39450adf058eb596f7bc9a
mectoys <mectoys2013@gmail.com>
2023-08-27 14:04:53 -0500

    video 10: Implementación de la herramienta NSSM para crear un Servicio de Windows para implementar en producción Crud Web

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from scr.route import route_attention_control
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    app.config.from_object(config['development'])
+   # app.config.from_object(config['development'])
     app.secret_key = 'Qwerty2023*'
 
     # Blueprint
     app.register_blueprint(route_attention_control.main, url_prefix='/')
 
-    app.run()
+    app.run(host='0.0.0.0' , port=50100, debug=True)
